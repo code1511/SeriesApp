938bb3f77703a217b1655f932b9efefefc221a2c
code1511 <anch20001511@gmail.com>
2021-05-14 16:12:59 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+import Card from './Card'
+function Head(){
+  return <h1 className="head">Top Netflix Series list</h1>
+}
+function Main(){
+  return(
+    <div className="content">
+      <Card name="DARK" val="https://picsum.photos/280/150"/>
+      <Card name="Extra Curricular" val="https://picsum.photos/270/150"/>
+      <Card name="Interstellar" val="https://picsum.photos/281/150"/>
+      <Card name="West-World"val="https://picsum.photos/280/149"/>
+      <Card name="Game Of Thrones" val="https://picsum.photos/280/151"/>
+      <Card name="Game Of Thrones" val="https://picsum.photos/282/151"/>
+      <Card name="Game Of Thrones" val="https://picsum.photos/278/151"/>
+      <Card name="Game Of Thrones" val="https://picsum.photos/281/152"/>
     </div>
   );
 }
-
-export default App;
+export {Head,Main}
---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Card(props) {
+    return (
+        <div className="card">
+            <img src={props.val} alt="this picture" className="img_c" />
+            <div className="card_info">
+            <span className="card_category">A Original Netflix Series. </span>
+            <h3 className="card_title">{props.name}</h3>
+            <a href="https://www.netflix.com/in/">
+                <button> watch  now</button>
+            </a>
+            </div>
+        </div>
+    )
+}
