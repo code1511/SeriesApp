89f0d3e1f5c4393b438f17f0ae97900a5aae85c1
Anchal Pandey <73632448+code1511@users.noreply.github.com>
2021-11-21 19:31:59 +0530

    fixed the error

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Card(props) {
     return (
         <div className="card">
-        //passing image values as props
+        /*passing image values as props
+        */
             <img src={props.val} alt="this picture" className="img_c" />
             <div className="card_info">
             <span className="card_category">A Original Netflix Series. </span>
